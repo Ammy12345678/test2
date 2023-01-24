4f4d00ee22c6c506a0f21fa80bb313bb36bf3a50
Anil Dhumal <Anil.Dhumal@cloudenergysoftware.com>
2023-01-24 23:58:57 +0530

    third test case

---
+++ src/test/java/com/Test/TC_003_loginFBpage3.java
@@ -0,0 +1,35 @@
+package com.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.PageObject.Loginpom;
+import com.Utility.BaseClass;
+import com.Utility.Library;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_003_loginFBpage3 extends BaseClass {
+
+	@Test
+	public void verifylogin()
+	{
+		
+		
+	Loginpom fis=PageFactory.initElements(driver, Loginpom.class);
+	Library.custom_sendkeys(fis.getEmailid(),"dfgh");
+	Library.custom_sendkeys(fis.getPassword(),"gftg");
+	Library.custom_click(fis.getButton());
+	
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+}
